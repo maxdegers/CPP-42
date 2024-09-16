fab2bac53ea0d2bf87b31f8ec17a960079f55070
maxdegers <maxdegers@gmail.com>
2024-09-16 09:30:10 +0200

    aded

---
+++ cpp-02/ex02/Fixed.cpp
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 12:56:52 by mbrousse          #+#    #+#             */
+/*   Updated: 2024/09/13 14:07:32 by mbrousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ */
+Fixed::Fixed(){
+    std::cout << "Default constructor called" << std::endl;
+}
+
+/**
+ * @brief Destroy the Fixed:: Fixed object
+ * 
+ */
+Fixed::~Fixed(){
+    std::cout << "Destructor called" << std::endl;
+}
+
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ * @param value 
+ */
+Fixed::Fixed( const int value) : _value(value << this->_fractBits){
+	std::cout << "Int constructor called" << std::endl;
+}
+
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ * @param value 
+ */
+Fixed::Fixed( const float value) : _value(roundf(value * (1 << this->_fractBits))){
+	std::cout << "Float constructor called" << std::endl;
+}
+
+/**
+ * @brief Construct a new Fixed:: Fixed object
+ * 
+ * @param cpy 
+ */
+Fixed::Fixed( const Fixed& cpy ){
+	std::cout << "Copy constructor called" << std::endl;
+	*this = cpy;
+}
+
+/**
+ * @brief 
+ * 
+ * @param src 
+ * @return Fixed& 
+ */
+Fixed& Fixed::operator=(const Fixed &src){
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &src)
+		this->_value = src.getRawBits();
+	return (*this);
+}
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int	Fixed::getRawBits(void) const{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_value);
+}
+
+/**
+ * @brief 
+ * 
+ * @param raw 
+ */
+void	Fixed::setRawBits(int const raw){
+	std::cout << "setRawBits member function called" << std::endl;
+	_value = raw;
+}
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int	Fixed::toInt( void ) const{
+	return (this->_value >> this->_fractBits);
+}
+
+/**
+ * @brief 
+ * 
+ * @return float 
+ */
+float Fixed::toFloat( void ) const{
+	return ((float)this->_value / (1 << this->_fractBits));
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed){
+	out << fixed.toFloat();
+	return (out);
+}
---
+++ cpp-02/ex02/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 12:56:59 by mbrousse          #+#    #+#             */
+/*   Updated: 2024/09/13 14:03:46 by mbrousse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int                 _value;
+	static const int    _fractBits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed& cpy);
+	Fixed(const int value);
+	Fixed(const float value);
+	
+	Fixed& operator=(const Fixed &src);
+
+	~Fixed();
+	
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat( void ) const;
+	int toInt( void ) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
